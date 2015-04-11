8ab5fafea65ce16dc6ff5045aabe658e2b4b0b8d
özgür <hinoglu@gmail.com>
2015-04-11 17:23:41 +0300

    some more cleaning up

--- myth-player.js
+++ myth-player.js
@@ -1,95 +1,3 @@
-
-// https://gist.github.com/mindeavor/13d4593b00b4ee7cea33
-// Partially apply arguments to a function. Useful for binding
-// specific data to an event handler.
-// Example:
-//
-//   var add = function (x,y) { return x + y; }
-//   var add5 = add.papp(5)
-//   add5(7) //=> 11
-//
-Function.prototype.papp = function () {
-  var slice = Array.prototype.slice
-  var fn = this
-  var args = slice.call(arguments)
-  return function () {
-    return fn.apply(this, args.concat(slice.call(arguments)))
-  }
-}
-
-// Prevents the default action. Useful for preventing
-// anchor click page jumps and form submits.
-// Example:
-//
-//   var x = 0;
-//   var increment = function () { x += 1 }
-//   myAnchorEl.addEventListener('click', increment.chill())
-//
-Function.prototype.chill = function() {
-  var fn = this
-  return function(e) {
-    e.preventDefault()
-    return fn()
-  };
-};
-
-// Both prevent default and partially apply in one go.
-// Example:
-//
-//   var x = 0;
-//   var increment = function (amount) { x += amount }
-//   myAnchorEl.addEventListener('click', increment.coldPapp(17))
-//
-Function.prototype.coldPapp = function() {
-  var slice = Array.prototype.slice
-  var fn = this
-  var args = slice.call(arguments)
-  return function(e) {
-    e.preventDefault()
-    return fn.apply(this, args.concat(slice.call(arguments, 1)))
-  };
-};
-
-// Apply the combination of two arrays of arguments.
-// Useful for dealing with fancy functions using the `arguments` keyword.
-// Example:
-//
-//   function sum () {
-//     var result = 0
-//     for (var i=0; i < arguments.length; i++) { result += arguments[i] }
-//     return result
-//   }
-//   function sumPlusTen () {
-//     return sum.apply2(null, [10], arguments)
-//   }
-//   sumPlusTen(100,200) //=> 310
-//
-Function.prototype.apply2 = function (context, args1, args2) {
-  var slice = [].slice
-  var args = slice.call(args1).concat(slice.call(args2))
-  return this.apply(context, args)
-}
-
-
-// Compose two functions together,
-// piping the result of one to the other.
-// Example:
-//
-//   var double = function (x) { return x * 2 }
-//   var addOne = function (x) { return x + 1 }
-//   var doublePlus = double.andThen(addOne)
-//   doublePlus(3) //=> 7
-//
-Function.prototype.andThen = function(f) {
-  var g = this
-  return function() {
-    var slice = Array.prototype.slice
-    var args = slice.call(arguments)
-    return f.call(this, g.apply(this, args))
-  }
-}
-
-
 var mythPlayer = function (selectorId, options, sources) {
     "use strict";
     options = options || {};
@@ -142,24 +50,24 @@ var mythPlayer = function (selectorId, options, sources) {
 
         self.getApi= function (mediaElement) {
             self.mediaElement = mediaElement;
-            return function(method, args, state){
-                args = Array.prototype.indexOf.call([null, undefined], args) != -1? undefined: args;
-                var retval = mediaElement[method].apply(mediaElement, args);
-                if(state){
-                    self.vm.state(state);
-                }
-                return retval;
+            // place holding for callbacks
+            return function(method, arg){
+                return mediaElement[method](arg);
             }
         };
 
         self.setSources = function (sources) {
+            // apply changes to dom
             m.startComputation();
             self.vm.sources(sources);
             m.endComputation();
         };
 
-        self.controller = function () {
-        };
+        self.setState = function(state){
+            self.vm.state(state);
+        }
+
+        self.controller = function () {};
 
         self.view = function (ctrl) {
             var vmSources = self.vm.sources(),
