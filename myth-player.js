3a06fcc65d158bf7137256ff1ba0ddb48fb7a0a3
özgür <hinoglu@gmail.com>
2015-04-13 16:51:54 +0300

    some more clean up

--- myth-player.js
+++ myth-player.js
@@ -1,7 +1,7 @@
 var mythPlayer = function (selectorId, options, sources) {
-    "use strict";
-    options = options || {};
-    sources = sources || {};
+    "use strict"
+    options = options || {}
+    sources = sources || {}
 
     var mythTools = (function () {
             /*
@@ -19,9 +19,9 @@ var mythPlayer = function (selectorId, options, sources) {
                     }
                 }
                 return copy;
-            };
+            }
 
-            var extend = function (arg1, arg2) {
+            , extend = function (arg1, arg2) {
                 var newObj = this.clone(arg1);
                 Object.keys(arg2).map(function (key) {
                     newObj[key] = arg2[key];
@@ -37,11 +37,20 @@ var mythPlayer = function (selectorId, options, sources) {
         self.defaultOptions = {
             autoplay: false
             ,preload: "auto"
-            ,controls: true
+            ,controls: false
         };
 
         self.vm = {
             init: function(){
+                var _config = options.config;
+                options.config = function (mediaElement, isInitialized, context) {
+                    if(isInitialized){
+                        return
+                    }
+                    self.api = self.api || self.getApi(mediaElement);
+                    self.api("load")
+                    return _config? _config(mediaElement, isInitialized, context): undefined;
+                }
                 this.sources = m.prop(sources);
                 this.options = m.prop(mythTools.extend(self.defaultOptions, options));
                 this.state = m.prop("INIT");
@@ -49,52 +58,47 @@ var mythPlayer = function (selectorId, options, sources) {
         };
 
         self.getApi= function (mediaElement) {
-            self.mediaElement = mediaElement;
+            self.mediaElement = mediaElement
             // place holding for callbacks
             return function(method, arg){
-                return mediaElement[method](arg);
+                var commands = {
+                    "playpause": function(){
+                        return mediaElement.paused? mediaElement.play(): mediaElement.pause()
+                    }
+                };
+                return method in commands? commands[method](arg): mediaElement[method](arg)
             }
-        };
+        }
 
         self.setSources = function (sources) {
             // apply changes to dom
-            m.startComputation();
-            self.vm.sources(sources);
-            m.endComputation();
-        };
-
-        self.setState = function(state){
-            self.vm.state(state);
+            m.startComputation()
+            self.vm.sources(sources)
+            m.endComputation()
         }
 
-        self.controller = function () {};
+        self.setState = function(state){
+            self.vm.state(state)
+        }
+
+        self.controller = function () {
+            self.vm.init()
+        }
 
         self.view = function (ctrl) {
             var vmSources = self.vm.sources(),
                 vmOptions = self.vm.options();
-
-            vmOptions.config = function (mediaElement, isInitialized, context) {
-                if(isInitialized){
-                    return
-                }
-                if (!self.api) {
-                    self.api = self.getApi(mediaElement);
-                }
-                self.api("load");
-            };
 
             var video = m("video", vmOptions, [
                             Object.keys(vmSources).map(function (key, idx) {
                                 return m("source", {type: "video/" + key, src: vmSources[key] });
                             })
                         ])
-            return [video]
+            return video
         };
 
-        self.vm.init();
-        m.module(document.getElementById(selectorId), self);
-        return self;
-    };
-    return new player();
+        m.module(document.getElementById(selectorId), self)
+        return self
+    }
+    return new player()
 };
-
